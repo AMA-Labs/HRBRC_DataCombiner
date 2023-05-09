29c9f6ceaefc2802b132e063f7659378aec0cba7
srenkesAMA <113476001+srenkesAMA@users.noreply.github.com>
2023-05-08 22:56:36 -0500

    pushing new code and updated command line interface

--- datacombiner.py
+++ datacombiner.py
@@ -2,36 +2,49 @@ import pandas as pd
 import os
 import re
 
+# Get the directory from the command-line argument (if provided)
+if len(sys.argv) > 1:
+    directory = sys.argv[1]
+else:
+    directory = '.'
+
 heart_file = None
 oxygen_file = None
+hrbrc_file = None
 
-for file in os.listdir("."):
-    if re.search("heart", file, re.IGNORECASE):
+# Get a list of files in the current directory
+files = os.listdir()
+
+# Loop through the files and find the first one that contains each of the specified strings
+for file in files:
+    if "heart" in file and not heart_file:
         heart_file = file
-        if oxygen_file is not None:
-            break  # both files found, exit loop
-    elif re.search("oxygen", file, re.IGNORECASE):
+    elif "oxygen" in file and not oxygen_file:
         oxygen_file = file
-        if heart_file is not None:
-            break  # both files found, exit loop
+    elif "HRBRC" in file and not hrbrc_file:
+        hrbrc_file = file
+    
 
 print("Heart rate file:", heart_file)
 print("Oxygen level file:", oxygen_file)
+print("HRBRC file:", hrbrc_file)
 
 df1=pd.read_csv(heart_file, header=1, na_values=[''], index_col=False)
 df2=pd.read_csv(oxygen_file, header=1, na_values=[''], index_col=False)
+df3=pd.read_csv(hrbrc_file, header=0)
+
+# Convert the date column to a datetime type
+df1['DateTime'] = pd.to_datetime(df1['com.samsung.health.heart_rate.end_time'])
+df2['DateTime'] = pd.to_datetime(df2['com.samsung.health.oxygen_saturation.end_time'])
+df3['DateTime'] = pd.to_datetime(df3['Date']+ ' ' + df3[' Time'])
 
 # Perform an outer join on date1 and date2
-combined_df = pd.merge(df1, df2, left_on='com.samsung.health.heart_rate.end_time', right_on='com.samsung.health.oxygen_saturation.end_time', how='outer')
-
-# Create a new column called combodate by combining date1 and date2
-combined_df["combodate"] = combined_df["com.samsung.health.heart_rate.end_time"].fillna(combined_df["com.samsung.health.oxygen_saturation.end_time"])
-
-# Drop the original date1 and date2 columns
-combined_df = combined_df.drop(["com.samsung.health.heart_rate.end_time", "com.samsung.health.oxygen_saturation.end_time"], axis=1)
+combined_df = pd.merge(df1, df2, left_on='DateTime', right_on='DateTime', how='outer')
+# Perform an outer join on date1 and date2
+combined_df = pd.merge(combined_df, df3, left_on='DateTime', right_on='DateTime', how='outer')
 
 # Sort the combined dataframe by combodate
-combined_df = combined_df.sort_values(by="combodate")
+combined_df = combined_df.sort_values(by="DateTime")
 
 # Write the sorted dataframe to a new CSV file
 combined_df.to_csv("combined_sorted.csv", index=False)
